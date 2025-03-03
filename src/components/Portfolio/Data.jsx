24ac76006725b9a17f8758eef4d26d3c23f939d1
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2025-03-03 14:08:24 +0100

    changes

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -1,4 +1,4 @@
-
+import project3 from "../../assets/images/p3.jpg";
 import project2 from "../../assets/images/p2.jpg";
 import project1 from "../../assets/images/p1.jpg";
 import project4 from "../../assets/images/p4.png";
@@ -6,6 +6,7 @@ import project4 from "../../assets/images/p4.png";
 export const projectsData = [
   {
     id: 3,
+    image: project3,
     title: "EngleWood Wine Merchants",
     href: "https://shero-website.vercel.app/",
   },
